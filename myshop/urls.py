ad0cb9dee7f20ebfd4ec8cbb7285039f87e80ab3
sakilanasrinsetu <setusakilanasrin@gmail.com>
2022-10-10 20:14:44 +0600

    update

---
+++ myshop/urls.py
@@ -0,0 +1,29 @@
+"""myshop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.apps import apps
+from django.urls import include, path
+from django.contrib import admin
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+
+    # The Django admin is not officially supported; expect breakage.
+    # Nonetheless, it's often useful for debugging.
+
+    path('admin/', admin.site.urls),
+
+    path('', include(apps.get_app_config('oscar').urls[0])),
+]
