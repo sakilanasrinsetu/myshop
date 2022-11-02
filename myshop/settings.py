b38b4f5c5051818befee329d232951e74d697c12
Tahseen-Zaman <tahseenzaman11@gmail.com>
2022-11-02 12:42:51 +0600

    Installed Django Oscar Promotions Lib

--- myshop/settings.py
+++ myshop/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -110,6 +110,10 @@ INSTALLED_APPS = [
     # 'apps.extension.apps.ExtensionConfig',
 
     # 'oscar.apps.customer.apps.CustomerConfig'
+
+    # Django Oscar Promotions Lib
+    'oscar_promotions.apps.PromotionsConfig',
+    'oscar_promotions.dashboard.apps.PromotionsDashboardConfig',
 ]
 
 MIDDLEWARE = [
