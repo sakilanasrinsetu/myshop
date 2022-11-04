8f16d8bdbfc0defe00c81e7f151a9be1e016d7c6
sakilanasrinsetu <setusakilanasrin@gmail.com>
2022-11-04 21:20:46 +0600

    mearge

--- myshop/settings.py
+++ myshop/settings.py
@@ -170,7 +170,7 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
