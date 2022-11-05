92f45bf479409dbfc7ba9d3a3e5379001bfd3053
sakilanasrinsetu <setusakilanasrin@gmail.com>
2022-11-05 10:13:27 +0600

    merge with hasan sojib

--- myshop/settings.py
+++ myshop/settings.py
@@ -178,6 +178,7 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
