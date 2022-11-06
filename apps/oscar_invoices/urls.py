15860a023f96e9a93aa5f0dc4d0a5afe36202ffb
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2022-11-06 21:37:13 +0600

    invoice api added
    OperationalError at /invoices/view
    no such table: oscar_invoices_invoice

--- apps/oscar_invoices/urls.py
+++ apps/oscar_invoices/urls.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from oscar.core.loading import get_class
-from .views import InvoicePreviewView, InvoiceAPIView
+from .views import *
 
 # InvoicePreviewView = get_class("oscar_invoices.views", "InvoicePreviewView")
 
@@ -8,8 +8,7 @@ from .views import InvoicePreviewView, InvoiceAPIView
 app_name = "oscar_invoices"
 urlpatterns = [
     # re_path(r"/(?P<pk>\d+)/", InvoicePreviewView.as_view(), name="invoice"),
-    path("invoice/", InvoiceAPIView.as_view(), name="invoice-api"),
-    path("/<int:pk>/", InvoicePreviewView.as_view(), name="invoice"),
-
-
+    path('/view', InvoiceListAPIView.as_view(), name="invoice"),
+    path('/view/<int:pk>', InvoiceDetailAPIView.as_view(), name="invoice"),
+    # path("/<int:pk>/", InvoicePreviewView.as_view(), name="invoice"),
 ]
--- apps/oscar_invoices/views.py
+++ apps/oscar_invoices/views.py
@@ -6,70 +6,74 @@ from oscar.core.loading import get_class, get_model
 from .serializers import InvoiceSerializer
 from rest_framework import generics, status
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 Invoice = get_model('oscar_invoices', 'Invoice')
 InvoiceCreator = get_class('oscar_invoices.utils', 'InvoiceCreator')
 
 
-class InvoicePreviewView(UserPassesTestMixin, SingleObjectMixin, View):
-    queryset = Invoice.objects.all()
+# class InvoicePreviewView(UserPassesTestMixin, SingleObjectMixin, View):
+#     queryset = Invoice.objects.all()
 
-    def test_func(self):
-        user = self.request.user
-        return user.is_staff
+#     def test_func(self):
+#         user = self.request.user
+#         return user.is_staff
 
-    def get(self, request, *args, **kwargs):
-        invoice = self.get_object()
-        rendered_invoice = InvoiceCreator().render_document(
-            invoice=invoice,
-            legal_entity=invoice.legal_entity,
-            order=invoice.order,
-            use_path=False,
-            request=request
-        )
-        return HttpResponse(rendered_invoice)
+#     def get(self, request, *args, **kwargs):
+#         invoice = self.get_object()
+#         rendered_invoice = InvoiceCreator().render_document(
+#             invoice=invoice,
+#             legal_entity=invoice.legal_entity,
+#             order=invoice.order,
+#             use_path=False,
+#             request=request
+#         )
+#         return HttpResponse(rendered_invoice)
 
 
 # Invoice API's
 
-class InvoiceAPIView(APIView):
-    permission_classes = (IsAuthenticated,)
+class InvoiceListAPIView(generics.ListAPIView):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
 
-    def get(self, request, *args, **kwargs):
-        invoice = Invoice.objects.get(pk=kwargs['pk'])
-        serializer = InvoiceSerializer(invoice)
+    def get_queryset(self):
+        queryset = Invoice.objects.all()
+        return queryset
+    def get(self, request, format=None):
+        invoices = Invoice.objects.all()
+        serializer = InvoiceSerializer(invoices, many=True)
         return Response(serializer.data)
-    
-    def put(self, request, *args, **kwargs):
-        invoice = Invoice.objects.get(pk=kwargs['pk'])
-        serializer = InvoiceSerializer(invoice, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, *args, **kwargs):
-        invoice = Invoice.objects.get(pk=kwargs['pk'])
-        invoice.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    def post(self, request, *args, **kwargs):
+
+    def post(self, request, format=None):
         serializer = InvoiceSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def patch(self, request, *args, **kwargs):
-        invoice = Invoice.objects.get(pk=kwargs['pk'])
-        serializer = InvoiceSerializer(invoice, data=request.data, partial=True)
+class InvoiceDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+    def get(self, request, pk, format=None):
+        invoice = self.get_object()
+        serializer = InvoiceSerializer(invoice)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        invoice = self.get_object()
+        serializer = InvoiceSerializer(invoice, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+    def delete(self, request, pk, format=None):
+        invoice = self.get_object()
+        invoice.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
